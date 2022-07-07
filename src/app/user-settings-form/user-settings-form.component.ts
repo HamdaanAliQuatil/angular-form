4664ac9fc20ac5873beba20b71b55cabda00f28a
HamdaanAliQuatil <q.hmd08@gmail.com>
2022-07-07 22:14:52 +0530

    feat: validate form using ngmodel and control events

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 import { UserSettings } from '../data/user-settings';
 
 @Component({
@@ -9,11 +10,11 @@ import { UserSettings } from '../data/user-settings';
 export class UserSettingsFormComponent implements OnInit {
 
   orignalUserSettings: UserSettings = {
-    name: 'Hamdaan',
-    emailOffer: true,
-    interfaceStyle: 'dark',
-    subscriptionType: 'pro',
-    notes: 'Demo'
+    name: null,
+    emailOffer: null,
+    interfaceStyle: null,
+    subscriptionType: null,
+    notes: null
   };
 
   userSettings: UserSettings = { ...this.orignalUserSettings };
@@ -21,6 +22,14 @@ export class UserSettingsFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onBlur(field: NgModel){
+    console.log('in onBlur', field.valid);
+  }
+
+  onSubmit(form: NgForm){
+    console.log('in onSubmit', form.valid);
   }
 
 }
